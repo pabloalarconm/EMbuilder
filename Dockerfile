43900637b8123ef242c325f0d23114089178c72b
pabloalarconm <pabloalarconmoreno@gmail.com>
2022-07-07 12:38:24 +0200

    minor changes before 0.1.0

--- Dockerfile
+++ Dockerfile
@@ -9,6 +9,3 @@ COPY ./requirements.txt /code/requirements.txt
 RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
 
 CMD ["uvicorn", "--host", "0.0.0.0", "embuilder.main:app"]
-# "--host", "127.0.0.1", "--port", "8000"
-
-# ENTRYPOINT ["python3","embuilder/api_test.py"]
--- test/test.py
+++ test/test.py
@@ -5,11 +5,12 @@ with open("data/data_test.json") as json_file:
     data = json.load(json_file)
 
 yarrrml = EMB(data["config"], data["prefixes"],data["triplets"])
+
 test_yarrrml = yarrrml.transform_ShEx()
 print(test_yarrrml)
-test_shex = yarrrml.transform_YARRRML()
-print(test_shex)
 test_obda = yarrrml.transform_OBDA()
 print(test_obda)
 test_sparql = yarrrml.transform_SPARQL()
 print(test_sparql)
+test_shex = yarrrml.transform_YARRRML()
+print(test_shex)
